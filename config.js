c1bab414d666fd69c06c9326d4020f0d3dbd348f
MAFIA ADEEL <adeelsabqi11@gmail.com>
2026-01-21 21:54:08 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,17 +2,17 @@
 
 
 const config = {
-  owner: ['94710632039', '94741336839'], // Owner numbers (no + sign)
-  botName: 'chatuwa xmd',            // Bot name
-  ownerName: 'CYBER NOVA TEAM',              // Owner name
+  owner: ['923174838990', '923131613251'], // Owner numbers (no + sign)
+  botName: 'ᗩᗪᗴᗴᒪ-᙭ᗰᗪ',            // Bot name
+  ownerName: 'ᗰᗩᖴIᗩ ᗩᗪᗴᗴᒪ',              // Owner name
   autoRead: true,                       // Auto read messages
   inboxBlock: false,                    // Block DMs?
   antiCall: true,                       // Block incoming calls?
   prefix: ['.', '!'],                   // Command prefixes
   sessionName: 'session',              // Session file name
   reactEmoji: '⚡',                     // Default reaction
-  footer: 'CYBER CHATU X BOT © 2026',     // Footer text
-  webLink: 'https://nova-force.mgx.world', // Website or contact link
+  footer: 'ᑭOᗯᗴᖇ ᗷY ᗰᗩᖴIᗩ ᗩᗪᗴᗴᒪ',     // Footer text
+  webLink: 'https://mafia-adeel.gt.tc', // Website or contact link
 };
 
 module.exports = config;
